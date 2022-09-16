87d3a79b382a35ec6f68c0d1194f4d99af2f8824
Adrian Arias <aarias.cocodin@gmail.com>
2022-09-16 16:27:41 +0200

    aprendiendo sobre estados del componente y Vue DevTools

---
+++ src/components/Counter.vue
@@ -0,0 +1,20 @@
+<template>
+  <h2>Counter</h2>
+  <p>{{ counter }} <sup>2</sup> = {{ counter * counter }}</p>
+
+</template>
+
+<script>
+export default {
+    // name: 'Patito'
+    data() {
+        return {
+            counter: 5
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
