74e3b086c2c72ae7195ada179e418ec3b1b53f8b
Adrian Arias <aarias.cocodin@gmail.com>
2022-09-21 16:34:46 +0200

    pruebas con jest - VueTest

--- tests/unit/components/counter.spec.js
+++ tests/unit/components/counter.spec.js
@@ -1,13 +1,67 @@
-import { shallowMount, mount } from '@vue/test-utils'
+import { shallowMount } from '@vue/test-utils'
 import Counter from '@/components/Counter'
 
 describe('Counter Component', () => {
 
-    test('debe de hacer match con el snapshot', () => {
+    let wrapper = shallowMount(Counter);
 
-        const wrapper = shallowMount(Counter)
+    beforeEach(() => {
+        wrapper = shallowMount(Counter)
+    })
 
-        expect(wrapper.html()).toMatchSnapshot()
+    // test('debe de hacer match con el snapshot', () => {
+
+    //     const wrapper = shallowMount(Counter)
+
+    //     expect(wrapper.html()).toMatchSnapshot()
+
+    // })
+
+    test('h2 debe de tener el valor por defecto "Counter" ', () => {
+
+        expect(wrapper.find('h2').exists()).toBe(true)
+
+        const h2Value = wrapper.find('h2').text()
+
+        expect(h2Value).toBe('Counter')
+    })
+
+    test('El valor por defecto debe de ser 100 en el p', async() => {
+        // pTags
+        //const pTags = wrapper.findAll('p')
+        const value = wrapper.find('[data-testid="counter"]').text()
+
+        // expect segundo p = 100
+        //expect(pTags[1].text()).toBe('100')
+        expect(value).toBe('100')
+
+    })
+
+    test('Debe incrementar y decrementar el contador', async() => {
+
+        const [increaseBtn, decreaseBtn] = wrapper.findAll('button')
+
+        await increaseBtn.trigger('click')
+        await increaseBtn.trigger('click')
+        await increaseBtn.trigger('click')
+        await decreaseBtn.trigger('click')
+        await decreaseBtn.trigger('click')
+
+        const value = wrapper.find('[data-testid="counter"]').text()
+
+        expect(value).toBe('101')
+
+    })
+
+    test('Debe de establecer el valor por defecto', () => {
+
+        const { start } = wrapper.props()
+
+        const value = wrapper.find('[data-testid="counter"]').text()
+
+        expect(value).toBe(start)
+
+        console.log(typeof start);
 
     })
 
